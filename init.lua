51f0709982791fef028caa255a0d71a8bbce44fc
Baru <216474718+6aru@users.noreply.github.com>
2026-06-15 14:55:44 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("core.options")
 require("core.keybinds")
 require("core.autocmds")
 
+require("themes.theme-loader").load()
+
 require("plugin-manager").setup()
 
 require("plugins.ui")
@@ -9,5 +11,5 @@ require("plugins.navigation")
 require("plugins.editing")
 require("plugins.treesitter")
 require("plugins.git")
+require("plugins.lint")
 require("plugins.todo")
-require("themes.theme-loader").load()
